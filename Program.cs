b3510042b6844aaebb3a54581c3414dd9180d067
Kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2020-05-23 06:09:37 +0530

    c# - basic stack

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Stack
+    {
+        private readonly List<object> _list;
+
+        public Stack()
+        {
+            _list = new List<object>();
+        }
+        public void Push(object obj)
+        {
+            if(obj == null) throw new InvalidOperationException("The input value is null");
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if(_list.Count == 0) throw new InvalidOperationException("Stack is empty");
+            var lastElement = _list[_list.Count - 1];
+            _list.RemoveAt(_list.Count - 1);
+            return lastElement;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
